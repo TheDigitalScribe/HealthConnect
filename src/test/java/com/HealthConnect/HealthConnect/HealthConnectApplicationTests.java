bad2572ff2481416bb3beec6eb0bdd7f2f3fca64
TheHaloEffect <githubemail165@gmail.com>
2025-03-17 13:31:39 +0000

    initial setup

---
+++ src/test/java/com/HealthConnect/HealthConnect/HealthConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HealthConnect.HealthConnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
